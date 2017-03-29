9674fb3b9ffef0c3d0d84c3a723971586dec8caa
Gianpaulo M. Soares <gsoares@uolinc.com>
2017-03-28 22:49:22 -0300

    Fixing function... Now I understood the problem

--- problem3.js
+++ problem3.js
@@ -6,34 +6,10 @@
  * What is the largest prime factor of the number 600851475143 ?
  */
 
-const isPrime = num => {
-	if(num % 2 === 0) return (n == 2)
-	if(num % 3 === 0) return (n == 3)
-
-	var s = Math.sqrt(num)
-
-	for(var i = 5; i <= s; i += 6) {
-		if(num % i       === 0) return false
-		if(num % (i + 2) === 0) return false
-	}
-
-	return true
-}
-
-const isEven = num => num % 2 === 0
-
 const lPrimeFactor = num => {
-	var st = ((isEven(num)) ? num : num -1) / 2
-	var i = (isEven(st)) ? st -1 : st
-
-	for(; i > 3; i -= 2){
-		//console.log(i);
-		if(isPrime(i) && num % i === 0) return i 
-	}
-	
+	let i = 2
+	while(i <= num) (num % i === 0) ? num /= i : i++
+	return i
 }
-
-console.log(isPrime(8))
-console.log(isPrime(29))
 
 console.log(lPrimeFactor(600851475143))
