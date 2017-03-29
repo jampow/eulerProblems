a1daf8e2302fd9addf46f1367602be3f74d6de87
Gianpaulo M. Soares <jam_pow@hotmail.com>
2017-03-29 19:24:33 -0300

    Fixing palindrome problem

--- problem4.js
+++ problem4.js
@@ -10,26 +10,22 @@
  */
 
 const lPalindrome = dig => {
-	let st = Math.pow(10, dig) - 1, a = st, b = st
+	let pals = [];
+
+	let st = Math.pow(10, dig) - 1
 	const lim = Math.pow(10, dig - 1)
 
-	while(a >= lim && b >= lim) {
-		let res,
-		mul = a * b,
-		pal = mul.toString().split('').reverse().join('') * 1
-		console.log(a,b, mul, pal);
-
-		if(mul === pal) 
-			res = mul 
-		else
-			if(a === lim) {
-				b--
-				a = st
-			} else
-				a--
-
-		if(res) return res
+	for(let i = st;i >= lim; i--) {
+		for(let j = st;j >= lim; j--) {
+			let prod = i * j
+			if(isPalindrome(prod))
+				pals.push(prod)
+		}
 	}
+
+	return Math.max.apply(null, pals)
 }
+
+const isPalindrome = num => num === num.toString().split('').reverse().join('') * 1
 
 console.log(lPalindrome(3))
