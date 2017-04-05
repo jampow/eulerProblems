15ba88f2217846034b938629a002c72b7b9c6fde
Gianpaulo M. Soares <jam_pow@hotmail.com>
2017-04-04 23:50:34 -0300

    first implementation

--- problem7.js
+++ problem7.js
@@ -8,4 +8,27 @@
  * What is the 10 001st prime number?
  */
 
+const isPrime = num => {
+	if(num === 2)
+		return true
+	else if(num % 2 === 0)
+		return false
+
+	let lim = Math.sqrt(num)
+	for(let i = 3; i < lim; i += 2)
+		if(num % i === 0) return false
+
+	return true
+}
+
+const findPrime = num => {
+	let c = 3
+	for(let i = 0; i <= num; i++, c += 2)
+		if(isPrime(c))
+			i += 1
+
+	return c
+}
+
+console.log(findPrime(6))
 
