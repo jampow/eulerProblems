e8787798a3121860331ca57c126663ac9e721dfb
Gianpaulo M. Soares <gsoares@uolinc.com>
2017-04-20 23:04:54 -0300

    Adding method to get numbers to the right

--- problem011.js
+++ problem011.js
@@ -54,3 +54,10 @@ const mtx = [
 	[20,73,35,29,78,31,90,01,74,31,49,71,48,86,81,16,23,57,05,54],
 	[01,70,54,71,83,51,54,69,16,92,33,48,61,43,52,01,89,19,67,48]
 ]
+
+const right = (mtx, x, y, qtd) =>{
+	if(x > mtx.length - 1 || y > mtx[x].length - qtd) return 0
+	return mtx[x].slice(y, y + qtd)
+}
+
+console.log(right(mtx, 0, 16, 4))
