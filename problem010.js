776a64c77c98099363be70b20e66e622e2044db7
Gianpaulo M. Soares <gsoares@uolinc.com>
2017-04-20 22:07:20 -0300

    Solving problem 10

--- problem010.js
+++ problem010.js
@@ -7,4 +7,27 @@
  * Find the sum of all the primes below two million.
  */
 
+const isPrime = num => {
+	if(num === 2) return true
 
+	if(num % 2 === 0) return false
+
+	let lim = Math.sqrt(num)
+
+	for(let i = 3; i <= lim; i += 2)
+		if(num % i === 0) return false
+
+	return true
+}
+
+const sumPrimes = lim => {
+	let primes = [2];
+
+	for(let i = 3; i < lim; i += 2)
+		if(isPrime(i)) primes.push(i)
+
+	return primes.reduce((prev, curr) => prev + curr)
+}
+
+
+console.log(sumPrimes(2e6))
