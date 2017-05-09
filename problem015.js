1d24e83e5824fcf1ff121e5fa002aa459d5456cc
Gianpaulo M. Soares <jam_pow@hotmail.com>
2017-05-08 23:45:46 -0300

    Fixing the problem description

--- problem015.js
+++ problem015.js
@@ -2,7 +2,7 @@
  * Problem 15
  * https://projecteuler.net/problem=15
  *
- * Starting in the top left corner of a 2×2 grid, and only being able to move 
+ * Starting in the top left corner of a 2x2 grid, and only being able to move 
  * to the right and down, there are exactly 6 routes to the bottom right corner.
  * 
  * How many such routes are there through a 20x20 grid?
