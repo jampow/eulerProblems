da4f027eb2a703cb18bb354fe124aee024589c3f
Gianpaulo M. Soares <jam_pow@hotmail.com>
2017-05-15 22:43:34 -0300

    adding some methods

--- problem016.js
+++ problem016.js
@@ -11,4 +11,24 @@ const sumDigits = num => num.toString()
 	.split('')
 	.reduce((prev, curr) => +prev + +curr)
 
-console.log(sumDigits(1234))
+const longMult = (_x, _y) => {
+	let xa = _x.split('').reverse()
+	let ya = _y.split('').reverse()
+
+	ya.forEach(function(y){
+		
+	})
+}
+
+const longPow = (base, exp) => {
+	prod = base.toString()
+	let i = 1
+
+	while(i < exp){
+		prod = longMult(prod, exp)
+		i++
+	}
+	return prod
+}
+
+console.log(sumDigits(Math.pow(2, 1000)))
