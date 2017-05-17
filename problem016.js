b4d1f2fc2a89649738b7ccd87eea54960fa9a214
Gianpaulo M. Soares <jam_pow@hotmail.com>
2017-05-16 23:49:50 -0300

    adding some methods

--- problem016.js
+++ problem016.js
@@ -14,10 +14,25 @@ const sumDigits = num => num.toString()
 const longMult = (_x, _y) => {
 	let xa = _x.split('').reverse()
 	let ya = _y.split('').reverse()
+	let partMult = []
 
-	ya.forEach(function(y){
-		
+	ya.forEach((y, i) => {
+		let dec = 0
+		partMult.push(new Array(i).fill(0))
+		xa.forEach((x) => {
+			let m = x * y + dec
+			m = m.toString().split('')
+			partMult[i].push(m.pop())
+			dec = m.join('') * 1
+		})
+		partMult[i].push(dec)
 	})
+	return partMult
+}
+
+const sumArr = arr => {
+	let max = Math.max.apply(null, arr.map(a => a.length))
+	return max
 }
 
 const longPow = (base, exp) => {
@@ -31,4 +46,5 @@ const longPow = (base, exp) => {
 	return prod
 }
 
-console.log(sumDigits(Math.pow(2, 1000)))
+//console.log(sumDigits(Math.pow(2, 1000)))
+console.log(sumArr(longMult('63', '622')))
