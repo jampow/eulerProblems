d526676fa345fa82ca9fd9fd9073acdc2643cec2
Gianpaulo M. Soares <gsoares@uolinc.com>
2017-05-30 22:09:07 -0300

    Saving progress...

--- problem017.js
+++ problem017.js
@@ -15,33 +15,56 @@
  */
 
 const numbers = {
-	1: 'one',
-	2: 'two',
-	3: 'three'
-	4: 'four',
-	5: 'five',
-	6: 'six',
-	7: 'seven',
-	8: 'eight',
-	9: 'nine',
-	10: 'ten',
-	11: 'eleven',
-	12: 'twelve',
-	13: 'thirteen',
-	14: 'fourteen',
-	15: 'fifteen',
-	16: 'sixteen',
-	17: 'seventeen',
-	18: 'eighteen',
-	19: 'nineteen',
-	20: 'twenty',
-	30: 'thirty',
-	40: 'fourty',
-	50: 'fivety',
-	60: 'sixty',
-	70: 'seventy',
-	80: 'eighty',
-	90: 'ninety',
-	100: 'hundred',
-	1000: 'thousand'
+	'1': 'one',
+	'2': 'two',
+	'3': 'three',
+	'4': 'four',
+	'5': 'five',
+	'6': 'six',
+	'7': 'seven',
+	'8': 'eight',
+	'9': 'nine',
+	'10': 'ten',
+	'11': 'eleven',
+	'12': 'twelve',
+	'13': 'thirteen',
+	'14': 'fourteen',
+	'15': 'fifteen',
+	'16': 'sixteen',
+	'17': 'seventeen',
+	'18': 'eighteen',
+	'19': 'nineteen',
+	'20': 'twenty',
+	'30': 'thirty',
+	'40': 'fourty',
+	'50': 'fivety',
+	'60': 'sixty',
+	'70': 'seventy',
+	'80': 'eighty',
+	'90': 'ninety',
+	'100': 'hundred',
+	'1000': 'thousand'
+};
+
+const writeNumber = number => {
+	let strNum = number.toString()
+	let arrNum = strNum.split('')
+
+	let result = ''
+
+	result += `${numbers[arrNum[0]]} `
+
+	switch (arrNum.length) {
+		case 4:
+			result += `${numbers['1000']} `
+		break
+		case 3:
+			result += `${numbers['100']} `
+		break
+
+	}
+
+	return result
 }
+
+console.log(writeNumber(342))
