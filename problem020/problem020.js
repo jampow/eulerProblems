795c50e7c702165d78725349c8c2da951b496003
Gianpaulo M. Soares <jam_pow@hotmail.com>
2017-07-08 11:14:42 -0300

    Solving problem 20

--- problem020/problem020.js
+++ problem020/problem020.js
@@ -2,8 +2,8 @@ const strMath = require('../util/strMath.js');
 
 const factorial = (n, acc) => {
 	if(!acc) acc = '1'
-	//if(n === '1')
-	console.log(n, acc, '\n')
+	if(n === '1')
+		console.log(n, acc, '\n')
 	if(n === '1')
 		return acc
 	else
@@ -13,3 +13,4 @@ const factorial = (n, acc) => {
 const sumDigits = n => n.split('').reduce((x,y) => +x + +y, 0)
 
 console.log(sumDigits(factorial('100')))
+
